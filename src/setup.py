f3ecda4be63f7803fdbcaffcb0ea4b90714d3da4
Julian Ceipek <julian.ceipek@gmail.com>
2012-11-13 22:29:52 -0500

    Made extension module work on OS X.

--- src/setup.py
+++ src/setup.py
@@ -3,30 +3,31 @@ from distutils.core import setup, Extension
 import os, sys
 
 libraries = ['OpenVG']
+extra_objects = list()
 if sys.platform.startswith('darwin'):
 	os.environ['LDFLAGS'] = '-g -framework GLUT -framework OpenGL -framework Cocoa'
 elif sys.platform.startswith('linux'):
 	libraries.extend(['glut', 'GL', 'GLU'])
-
+	extra_objects.extend([
+		'libOpenVG_la-shArrays.o',
+		'libOpenVG_la-shContext.o',
+		'libOpenVG_la-shExtensions.o',
+		'libOpenVG_la-shGeometry.o',
+		'libOpenVG_la-shImage.o',
+		'libOpenVG_la-shPaint.o',
+		'libOpenVG_la-shParams.o',
+		'libOpenVG_la-shPath.o',
+		'libOpenVG_la-shPipeline.o',
+		'libOpenVG_la-shVectors.o',
+		'libOpenVG_la-shVgu.o'])
  
 pyshiva_module = Extension('rect', 
 	include_dirs = ['.','../include','/usr/local/include'],
 	sources = ['test_rect.c','pyShivamodule.c'],
 	libraries = libraries,
-	extra_objects = [
-	'libOpenVG_la-shArrays.o',
-	'libOpenVG_la-shContext.o',
-	'libOpenVG_la-shExtensions.o',
-	'libOpenVG_la-shGeometry.o',
-	'libOpenVG_la-shImage.o',
-	'libOpenVG_la-shPaint.o',
-	'libOpenVG_la-shParams.o',
-	'libOpenVG_la-shPath.o',
-	'libOpenVG_la-shPipeline.o',
-	'libOpenVG_la-shVectors.o',
-	'libOpenVG_la-shVgu.o'])
- 
-setup (name = 'PackageName',
+	extra_objects = extra_objects)
+
+setup (name = 'pyShiva',
         version = '1.0',
-        description = 'This is a demo package',
+        description = 'Test package for pyShiva',
         ext_modules = [pyshiva_module])
