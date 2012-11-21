cac69a64e0fdb61729ed2f11292d56cd2f8af4c7
Julian Ceipek <julian.ceipek@gmail.com>
2012-11-21 01:01:13 -0500

    Improving Linux support?

--- wrapper/setup.py
+++ wrapper/setup.py
@@ -11,20 +11,12 @@ if sys.platform.startswith('darwin'):
     os.environ['LDFLAGS'] = '-g -framework OpenGL -framework Cocoa ../glfw-2.7.7/lib/cocoa/libglfw.a ../ShivaVG/src/libOpenVG.a'
 
 elif sys.platform.startswith('linux'):
-    libraries.extend(['glut', 'GL', 'GLU'])
+    libraries.extend(['GL'])
 
     extra_objects.extend([
-        '../src/libOpenVG_la-shArrays.o',
-        '../src/libOpenVG_la-shContext.o',
-        '../src/libOpenVG_la-shExtensions.o',
-        '../src/libOpenVG_la-shGeometry.o',
-        '../src/libOpenVG_la-shImage.o',
-        '../src/libOpenVG_la-shPaint.o',
-        '../src/libOpenVG_la-shParams.o',
-        '../src/libOpenVG_la-shPath.o',
-        '../src/libOpenVG_la-shPipeline.o',
-        '../src/libOpenVG_la-shVectors.o',
-        '../src/libOpenVG_la-shVgu.o'])
+        '../glfw-2.7.7/lib/x11/libglfw.a',
+        '../ShivaVG/src/libOpenVG.a'])
+
 
  
 pyshiva_module = Extension('pyshiva',
