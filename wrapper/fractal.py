dedf174727254965342c4194b6b57f6b4de79794
Julian Ceipek <julian.ceipek@gmail.com>
2012-12-10 03:09:40 -0500

    Added a fractal demo.

---
+++ wrapper/fractal.py
@@ -0,0 +1,23 @@
+'''
+Fractal Demo.
+'''
+
+import pyshiva as ps
+from math import sin, cos
+
+w = ps.Window(title = "Fractals!")
+
+def create_circles(iterations, x, y, radius):
+	w.add(ps.Circle(x,y,radius,(abs(sin(radius)),0,abs(cos(radius)),0.2)))
+	if iterations > 0:
+		iterations -= 1
+		factor = 2.4
+		create_circles(iterations, x, y + radius/factor, radius/factor)
+		create_circles(iterations, x, y - radius/factor, radius/factor)
+		create_circles(iterations, x + radius/factor, y, radius/factor)
+		create_circles(iterations, x - radius/factor, y, radius/factor)
+
+create_circles(5,w.width/2,w.height/2,w.height/2)
+
+while w.is_open():
+	w.refresh()
