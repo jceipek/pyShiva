b561dee2ddb2d90f9bbd2c931f9408e5df479774
Julian Ceipek <julian.ceipek@gmail.com>
2012-12-10 02:41:04 -0500

    Made rose curves 2 memory efficient by changing color rather than making a new one.

--- wrapper/color_management.py
+++ wrapper/color_management.py
@@ -15,22 +15,24 @@ for i in range(500):
     a = abs(math.cos(i))*0.5
     side_length = abs(math.sin(i))*50
     r = ps.Rect(0,0,side_length,side_length,(r,abs(math.sin(i)),1,0.1))
-    w.add(r) # Add the squares to the window.
+    w.add(r) # Add the rectangles to the window.
 
-k = 0.25 # k is the type of rose curve
+k = 0.25
 while w.is_open():
     x,y = ps.get_mouse_pos()
     t = w.s_since_open()*2 # Use a scaled time since program start as the parametric time value
     radius = abs(math.sin(w.s_since_open()))
     if radius < 0.01: # Every time the curve collapses...
         k = random.random() # Randomize the k value to change the type of the curve
-
+    #k = float(x)/w.width
+    #print k
+    #k = 0.760935
     # Place every rectangle along a rose curve, offset by its index
     for (i,r) in enumerate(w):
         ran = random.random()
         r.x = radius*math.cos(k*(t+i))*math.sin(t+i)*w.width/2+w.width/2
         r.y = radius*math.sin(k*(t+i))*math.sin(t+i)*w.height/2+w.height/2
-        r.color.values = (math.cos(t/10.0), math.sin(t/10.0), math.sin(ran*i)/2.0 + .5, 0.1)
+        r.color = (math.cos(t/10.0), math.sin(t/10.0), math.sin(ran*i)/2.0 + .5, 0.1)
 
     # Update the screen
     w.refresh()
