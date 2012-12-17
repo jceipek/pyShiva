90ca1ff0fb48a4f9207b1fe46edbb954bef1fa2b
berit <berit.johnson@students.olin.edu>
2012-12-17 00:46:43 -0500

    updated demos- made pong more pong-like

--- wrapper/paddle.py
+++ wrapper/paddle.py
@@ -1,14 +1,29 @@
 import pyshiva as ps
+import random
 
 class Paddle(ps.Rect):
 	def __init__(self, y, window):
 		self.window = window
-		ps.Rect.__init__(self, 50, y+50, 20, 100, (0,0,1,0.5))
+		ps.Rect.__init__(self, 50, y+50, 20, 100, (0,0,1,1))
 		window.add(self)
 
 	def simulate(self, t):
 		x,y = ps.get_mouse_pos()
 		self.y = y - self.height/2
+
+class Enemy(ps.Rect):
+	def __init__(self, y, window):
+		self.window = window
+		self.speed = 100
+		ps.Rect.__init__(self, window.width-70, y+50, 20, 100, (1,0,0,1))
+		window.add(self)
+
+
+	def simulate(self, t, ypos):
+		#x,y = ps.get_mouse_pos()
+		self.y  = ypos - self.height/2
+
+
 
 class Ball(ps.Rect):
 	def __init__(self, window):
@@ -19,13 +34,17 @@ class Ball(ps.Rect):
 		ps.Rect.__init__(self, window.height/2, window.width/2, 20, 20)
 		window.add(self)
 
+	def change_color(self):
+		self.color = (random.random(),random.random(), random.random())
+
 	def simulate(self, t, colliders):
 		delta_x = self.dir_x * self.speed * t
 		delta_y = self.dir_y * self.speed * t
 		for c in colliders:
-			if c.x+c.width > self.x+delta_x and c.x+delta_x < self.x:
-				if c.y+c.height > self.y+delta_y+self.height and c.y < self.y+delta_y:
-					self.dir_x = 1
+			if (self.x + delta_x < c.x+c.width < self.x) or (self.x + self.width < c.x < self.x + self.width + delta_x):
+				if c.y - self.height/2 < self.y + delta_y < c.y + c.height + self.height/2:
+					self.dir_x *= -1
+					self.change_color()
 					return
 		if self.x + delta_x < 0:
 			self.dir_x = 1
@@ -47,13 +66,16 @@ class Ball(ps.Rect):
 w = ps.Window()
 
 paddle = Paddle(w.height/2, w)
+enemy_paddle = Enemy(w.height/2, w)
 ball = Ball(w)
 
 colliders = list()
 colliders.append(paddle)
+colliders.append(enemy_paddle)
 
 while w.is_open():
 	t = w.s_since_refresh()
 	paddle.simulate(t)
+	enemy_paddle.simulate(t, ball.y)
 	ball.simulate(t, colliders)
 	w.refresh()
